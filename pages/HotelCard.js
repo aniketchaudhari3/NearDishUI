551a9b05185d606439b3a0e51a3dfcd75cd7d396
TejasChaudhariNdb <tejaschaudhari038@gmail.com>
2020-09-24 00:02:26 +0530

    promoted

--- pages/HotelCard.js
+++ pages/HotelCard.js
@@ -2,22 +2,21 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
-import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
 
+import Typography from '@material-ui/core/Typography';
+import Chip from '@material-ui/core/Chip';
 
 import Rating from '@material-ui/lab/Rating';
-
+import Hidden from '@material-ui/core/Hidden';
 
 import Alert from '@material-ui/lab/Alert';
 
 const useStyles = makeStyles({
 
   media: {
-    height: 140,
+    height: 115,
   },
 });
 
@@ -26,6 +25,13 @@ export default function HotelCard(props) {
     const classes = useStyles();
     // const [value, setValue] = React.useState(4);
     console.log(props)
+    var promote;
+if(props.promote === true){
+promote =   <Chip  color='primary' size='small' label='Promoted' className='promoted'/>
+
+ }else{
+   promote = null;
+ }
     return (
         <>
 
@@ -37,6 +43,8 @@ export default function HotelCard(props) {
           image="https://images.immediate.co.uk/production/volatile/sites/30/2020/08/mexican-chicken-burger_1-b5cca6f.jpg?quality=90&resize=440%2C400"
           title="Contemplative Reptile"
         />
+{promote}
+
         <CardContent>
      
           <Typography gutterBottom variant="h6" component="h6">
--- pages/index.js
+++ pages/index.js
@@ -12,31 +12,43 @@ export default function Home() {
     { id:1,
       name: "Gajraj Park",
       Type: "Indian Pure Veg",
+      promote:true,
+      rating:5,
     },
     {
       id:2,
       name: "Hera Garden",
       Type: "Mughlai, Biryani, Punjabi, Chinese",
+      promote:false,
+      rating:2,
     },
     {
       id:3,
       name: "Hotel Garjana",
       Type: "North Indian, Thalis",
+      promote:true,
+      rating:5,
     },
     {
       id:4,
       name: "Hera",
       Type: "Fast Food, Maharashtrian, North Indian",
+      promote:false,
+      rating:4,
     },
     {
       id:5,
       name: "Kaka ka Dabha",
       Type: "Indian, Chinese",
+      promote:false,
+      rating:3,
     },
     {
       id:6,
       name: "Hotel Raj",
       Type: "Indian, Chinese",
+      promote:true,
+      rating:2,
     }
   ];
 
@@ -69,7 +81,7 @@ export default function Home() {
 Hotels.map((Hotel, index) => (
 <Grid  key={index} item xs >
          
-<HotelCard key={Hotel.id} HotelName={Hotel.name} rating={index} Type={Hotel.Type} />
+<HotelCard key={Hotel.id} HotelName={Hotel.name} rating={Hotel.rating} Type={Hotel.Type} promote={Hotel.promote} />
 
 </Grid> 
 
