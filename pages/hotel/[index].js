8d1345b195b65da9c09244feea7047db3607a60c
TejasChaudhariNdb <tejaschaudhari038@gmail.com>
2020-11-09 15:28:56 +0530

    done

--- pages/hotel/[index].js
+++ pages/hotel/[index].js
@@ -192,6 +192,7 @@ Chai Tapri
       >
         <MenuItem onClick={handleClose}>Profile</MenuItem>
         <MenuItem onClick={handleClose}>My account</MenuItem>
+        <MenuItem onClick={handleClose}>My account</MenuItem>
    
         <MenuItem onClick={handleClose}>Logout</MenuItem>
         <MenuItem onClick={handleClose}>Logout</MenuItem>
